de898a7a4c091f9a751d88d7ea02cf5fc97bb3f6
Bryan Hatton <bryanjhatton@gmail.com>
2015-06-15 14:05:23 -0400

    Relax ForceCast severity to trigger a warning instead of an error

--- Source/SwiftLintFramework/Rules/ForceCastRule.swift
+++ Source/SwiftLintFramework/Rules/ForceCastRule.swift
@@ -17,7 +17,7 @@ public struct ForceCastRule: Rule {
         return file.matchPattern("as!", withSyntaxKinds: [.Keyword]).map { range in
             return StyleViolation(type: .ForceCast,
                 location: Location(file: file, offset: range.location),
-                severity: .High,
+                severity: .Medium,
                 reason: "Force casts should be avoided")
         }
     }
